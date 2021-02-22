8b9012cdb13e7fc72f228d2a1962dba2c538714e
Oksana <solomkao@gmail.com>
2021-02-22 19:45:53 +0200

    Initial commit

---
+++ src/main/java/com/solomka/Homewok22MongodbSpringApplication.java
@@ -0,0 +1,13 @@
+package com.solomka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homewok22MongodbSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homewok22MongodbSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solomka/Homewok22MongodbSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solomka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homewok22MongodbSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
